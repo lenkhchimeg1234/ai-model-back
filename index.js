a8d27c4d0c286b51faebafc9cb2f1cb23773f70e
25LP8702 25LP8702 <25LP8702@G302-CC0089.local>
2026-02-26 10:50:52 +0800

    done33

--- index.js
+++ index.js
@@ -3,29 +3,24 @@ const cors = require("cors");
 const multer = require("multer");
 require("dotenv").config();
 
-
 const getImageAnalysis = require("./image-analysis");
 const getImageCreator = require("./imagecreator");
 const getTextGenerator = require("./textgeneration");
-const storage = multer.memoryStorage();
+const ChatBot = require("./chatbot");
 
-const upload = multer({ storage: storage });
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors());
-
 app.use(express.json());
 
 app.post("/analyze-image", upload.single("image"), getImageAnalysis);
 app.post("/create-image", getImageCreator);
 app.post("/generate-text", getTextGenerator);
-app.post("/Chat-Bot", ChatBot);
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome to the Image Analysis API" });
-// });
+app.post("/Chat-Bot", ChatBot);   // ← ЭНЭГ НЭМНЭ
 
 app.listen(PORT, () => {
   console.log(`API listening on http://localhost:${PORT}`);
